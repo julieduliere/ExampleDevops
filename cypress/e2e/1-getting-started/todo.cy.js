d1306db5889728373d588a92a19731731d85ad89
julieduliere <julie.duliere@outlook.fr>
2024-07-26 14:46:34 +0200

    coucou commit repo 2

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -5,7 +5,7 @@
 // This spec file contains a variety of sample tests
 // for a todo list app that are designed to demonstrate
 // the power of writing tests in Cypress.
-//
+//coucou
 // To learn more about how Cypress works and
 // what makes it such an awesome testing tool,
 // please read our getting started guide:
